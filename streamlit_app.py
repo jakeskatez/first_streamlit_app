42c1777095d6b5f8566ea9d8a9846b7016948c82
jakeskatez <54611384+jakeskatez@users.noreply.github.com>
2022-06-17 14:08:22 -0700

    Update streamlit_app.py
    new section to display fruityvice api and added user choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -22,6 +22,11 @@ fruits_to_show  = my_fruit_list.loc[fruits_selected]
 
 #display the data
 streamlit.dataframe(fruits_to_show)
+
+#header for fruit advice & user input
+streamlit.header('Fruityvice Fruit Advice')
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlit.write('The user entered', fruit_choice)
 
 # take json and normalize
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
